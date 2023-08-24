9345808d047bc1f444625eb9b2429082f0c524e9
Felipe Torlai <felipe.c.torlai@gmail.com>
2023-08-23 22:47:48 -0300

    cp1

---
+++ src/app/components/button.js
@@ -0,0 +1,9 @@
+export default function Button({ children, ...props }) {
+    return (
+        // eslint-disable-next-line react/no-unescaped-entities
+        <button className="button">{text}"
+            {children}
+            <Button>Clique aqui</Button>
+        </button>
+    )
+}
---
+++ src/app/components/title.js
@@ -0,0 +1,8 @@
+export default function CompomenteTitle() {
+    return (
+    // eslint-disable-next-line react/no-unescaped-entities
+    <title className="title">{text};"
+    {children}
+</title>
+    );
+}
